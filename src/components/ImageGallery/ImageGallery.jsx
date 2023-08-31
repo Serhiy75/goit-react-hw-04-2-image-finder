6654926b16d5d024cc1708b1f107540c3ca59d88
sstfnk3107 <s.stefanyk75@gmail.com>
2023-08-31 21:51:02 +0300

    123

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,58 +1,60 @@
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
 import { Modal } from 'components/Modal/Modal';
-import { Component, useEffect, useState } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
-// export const ImageGallery = () => {
-//   const [isModalShow, setIsModalShow] = useState(false);
-//   const [largeImageURL, setLargImageUrl] = useState('');
-//   const [tags, setTags] = useState('');
-// }
+export const ImageGallery = ({ hits }) => {
+  const [isModalShow, setIsModalShow] = useState(false);
+  const [largeImageURL, setLargImageUrl] = useState('');
+  const [tags, setTags] = useState('');
 
-// useEffect(() => {});
+  // useEffect(() => {});
 
-// const handleToggle = (largeImageURL, tags) =>{}
-
-export class ImageGallery extends Component {
-  state = {
-    isModalShow: false,
-    largeImageURL: '',
-    tags: '',
+  const handleToggle = (largeImageURL, tags) => {
+    setIsModalShow(!isModalShow);
+    setLargImageUrl(largeImageURL);
+    setTags(tags);
   };
 
-  handleToggle = (largeImageURL, tags) => {
-    this.setState(prevState => ({
-      isModalShow: !prevState.isModalShow,
-      largeImageURL,
-      tags,
-    }));
-  };
+  // export class ImageGallery extends Component {
+  //   state = {
+  //     isModalShow: false,
+  //     largeImageURL: '',
+  //     tags: '',
+  //   };
 
-  render() {
-    const { hits } = this.props;
-    return (
-      <>
-        <ul className="ImageGallery">
-          {hits.map(({ id, webformatURL, tags, largeImageURL }) => (
-            <ImageGalleryItem
-              onClick={() => this.handleToggle(largeImageURL, tags)}
-              webformatURL={webformatURL}
-              tags={tags}
-              key={id}
-            />
-          ))}
-        </ul>
-        {this.state.isModalShow && (
-          <Modal
-            largeImageURL={this.state.largeImageURL}
-            tags={this.state.tags}
-            closeModal={this.handleToggle}
+  //   handleToggle = (largeImageURL, tags) => {
+  //     this.setState(prevState => ({
+  //       isModalShow: !prevState.isModalShow,
+  //       largeImageURL,
+  //       tags,
+  //     }));
+  //   };
+
+  // render() {
+  // const { hits } = this.props;
+  return (
+    <>
+      <ul className="ImageGallery">
+        {hits.map(({ id, webformatURL, tags, largeImageURL }) => (
+          <ImageGalleryItem
+            onClick={() => handleToggle(largeImageURL, tags)}
+            webformatURL={webformatURL}
+            tags={tags}
+            key={id}
           />
-        )}
-      </>
-    );
-  }
-}
+        ))}
+      </ul>
+      {isModalShow && (
+        <Modal
+          largeImageURL={largeImageURL}
+          tags={tags}
+          closeModal={handleToggle}
+        />
+      )}
+    </>
+  );
+};
 
 ImageGallery.propTypes = {
   hits: PropTypes.array.isRequired,
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,50 +1,49 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
+// import { Component } from 'react';
 
-// import { useEffect } from 'react';
+import { useEffect } from 'react';
 
-// export const Modal = ({ closeModal, src, alt }) => {
-//   useEffect(() => {
-//     const handleEscape = e => {
-//       if (e.key === 'Escape') {
-//         closeModal({ src: '', alt: '' });
-//       }
-//     };
-//     window.addEventListener('keydown', this.handleEscape);
-//     return () => {
-//       window.removeEventListener('keydown', this.handleEscape);
-//     };
-//   }, [closeModal]);
-// };
+export const Modal = ({ closeModal, tags, largeImageURL }) => {
+  useEffect(() => {
+    const handleEscape = e => {
+      if (e.key === 'Escape') {
+        closeModal({ src: '', alt: '' });
+      }
+    };
+    window.addEventListener('keydown', handleEscape);
+    return () => {
+      window.removeEventListener('keydown', handleEscape);
+    };
+  }, [closeModal]);
 
-export class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleEscape);
-  }
+  // export class Modal extends Component {
+  //   componentDidMount() {
+  //     window.addEventListener('keydown', this.handleEscape);
+  //   }
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleEscape);
-  }
-  handleEscape = e => {
-    if (e.key === 'Escape') {
-      this.props.closeModal({ src: '', alt: '' });
-    }
-  };
-  render() {
-    return (
-      <div
-        className="Overlay"
-        onClick={evt => {
-          if (evt.target === evt.currentTarget) this.props.closeModal();
-        }}
-      >
-        <div className="Modal">
-          <img src={this.props.largeImageURL} alt={this.props.tags} />
-        </div>
+  //   componentWillUnmount() {
+  //     window.removeEventListener('keydown', this.handleEscape);
+  //   }
+  //   handleEscape = e => {
+  //     if (e.key === 'Escape') {
+  //       this.props.closeModal({ src: '', alt: '' });
+  //     }
+  //   };
+  // render() {
+  return (
+    <div
+      className="Overlay"
+      onClick={evt => {
+        if (evt.target === evt.currentTarget) closeModal();
+      }}
+    >
+      <div className="Modal">
+        <img src={largeImageURL} alt={tags} />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
 Modal.propTypes = {
   closeModal: PropTypes.func.isRequired,
   largeImageURL: PropTypes.string.isRequired,
